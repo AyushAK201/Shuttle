3969fb443584057d2deeec6ada02688085b3f8e4
AyushAK201 <107787211+AyushAK201@users.noreply.github.com>
2026-05-27 05:24:10 +0530

    Module 1 commits lecture 1.4 (#1) (#3)

--- src/main/java/com/example/Module1/Module1Application.java
+++ src/main/java/com/example/Module1/Module1Application.java
@@ -1,8 +1,6 @@
 package com.example.Module1;
 
-import com.example.Module1.impl.EmailNotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,20 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Module1Application implements CommandLineRunner {
 
- //   @Autowired  // This basically tell the spring that if we have the bean of PaymentService call please inject it here
-    final NotificationService notifObj;  //injecting dependency
+    @Autowired  // This basically tell the spring that if we have the bean of PaymentService call please inject it here
+    PaymentService paymentServiceObj;
 
-    public Module1Application(NotificationService notficSrvObj)
-    {
-       this.notifObj=notficSrvObj;   //constructor dependency injection
-    }
 	public static void main(String[] args) {
 		SpringApplication.run(Module1Application.class, args);
 	}
 
     @Override
     public void run(String... args) throws Exception {
-    //  notifObj=new EmailNotificationService();
-      notifObj.send("Hello");
+        paymentServiceObj.Pay();
     }
 }
--- src/main/java/com/example/Module1/impl/EmailNotificationService.java
+++ src/main/java/com/example/Module1/impl/EmailNotificationService.java
@@ -1,14 +1,7 @@
 package com.example.Module1.impl;
 
 import com.example.Module1.NotificationService;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
 
-//@Primary  //If there are multiple beans available then the one with Primary would be executed
-@Component
-@ConditionalOnProperty(name = "notification.type" , havingValue="email")
 public class EmailNotificationService implements NotificationService {
     @Override
     public void send(String msg) {
